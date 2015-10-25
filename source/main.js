f42055c2e063be3040dc857c35b07b28d379dd30
wohaiwo <zhome621@gmail.com>
2015-10-25 20:52:55 +0800

    verison 2.0

--- source/main.js
+++ source/main.js
@@ -1,18 +1,36 @@
 
-var score = 0;   // 玩家分数
-var board = new Array();  // 存放页面布局数据
-var hasConflicted  = new Array();	// 来判断该位置是否已经重叠过
+
+var score = 0; 						 			 // 玩家分数
+var bestScore = 0;								 // 最高分
+var board = new Array(); 						 // 存放页面布局数据
+var hasConflicted  = new Array();				 // 来判断该位置是否已经重叠过	防止出现多次方格叠加的问题	
+var documentWdith = window.innerWidth;		   	 // 获得屏幕的大小 设置自适应尺寸
+var gridContainerWidth = 0.92 * documentWdith;	 
+var cellSideLength = 0.18 * documentWdith;		 // 单个方格的宽度
+var cellSpace = 0.04 * documentWdith;
+
 window.onload = function() {
-	//  全局变量
+	prepareForMobile();
 	newGame();
 };
 
+// 监听游戏开始事件
+document.getElementById("new-game-button").addEventListener("click", function(event) {
+	event.preventDefault();
+	newGame();
+}, false);
+document.getElementById("new-game-button").addEventListener("touchstart", function(event) {
+	newGame();
+})
+
 // 响应用户的按键 移动数字
-$(document).keydown(function (ev) {
-	switch(ev.keyCode) {
+document.addEventListener("keydown", function(event) {
+	// 取消键盘方向键的默认行为 
+	event.preventDefault();
+	switch(event.keyCode) {
 		case 37:  // left
-			if(moveLeft()) {		// 先判断是否可以向左移动 然后进行移动 如果移动成功了就返回true
-				setTimeout("generateOneCell(), isGameOver()",100);	// 移动后判断随机生成一个数字
+			if(moveLeft()) {		
+				setTimeout("generateOneCell(), isGameOver()",100);	
 			}
 			break;
 		case 38:  // up
@@ -35,6 +53,53 @@ $(document).keydown(function (ev) {
 	}
 });
 
+// 手机触屏是的起点，终点的坐标
+var startX, startY;
+document.addEventListener("touchstart", function(event) {
+	event.preventDefault();							// 消除手指滑动的触发的事件
+	startX = event.touches[0].pageX;
+	startY = event.touches[0].pageY;
+}, false);
+document.addEventListener("touchend", function(event) {
+	endX = event.changedTouches[0].pageX;
+	endY = event.changedTouches[0].pageY;
+	mobileMove(endX, endY);
+}, false);
+
+function mobileMove(endX, endY) {
+	var X = endX - startX;
+	var Y = endY - startY;
+	console.log(Math.abs(X) + " " + Math.abs(Y) + " " + 0.05*documentWdith);
+	// 手指单击的时候会发生触发 "touchend"事件, 这里设置一个手指移动距离来防止这个问题
+	if(Math.abs(X) < 0.2 * documentWdith && Math.abs(Y) < 0.2 * documentWdith) {
+		return false;
+	}
+	if(Math.abs(X) > Math.abs(Y)) {
+		if(X > 0) {
+		// 向右边移动
+			if(moveRight()) {		
+				setTimeout("generateOneCell(), isGameOver()",100);
+			}
+		} else {
+			// 向左边移动
+			if(moveLeft()) {
+				setTimeout(	"generateOneCell(), isGameOver()",100);
+			}
+		}
+	} else {
+		if(Y > 0) {
+			// 向下边移动
+			if(moveDown()) {
+				setTimeout(	"generateOneCell(), isGameOver()",100);
+			}
+		} else {
+			// 向上边移动
+			if(moveUp()) {
+				setTimeout(	"generateOneCell(), isGameOver()",100);
+			}
+		}
+	}
+}
 
 
 
--- source/support2048.js
+++ source/support2048.js
@@ -6,8 +6,13 @@
 * @description 修复了移动化 兼容手机的bug
 */
 
+
 /* 开始游戏 */
 function newGame() {
+	// 重置游戏分数
+	score = 0;
+	updateScore(score);
+
 	// 初始化棋盘格
 	init();      
 	// 随机输出两个方格
@@ -17,12 +22,16 @@ function newGame() {
 
 // 初始化网格
 function init() {
-	for(var i = 0; i < 4; i++) 
+	for(var i = 0; i < 4; i++) {
 		for(var j = 0; j < 4; j++) {
 			var gridCell = document.getElementById("grid-cell-"+ i +"-" + j);
 			gridCell.style.left = getPos(i, j)[0] + "px";
 			gridCell.style.top = getPos(i, j)[1] + "px";
-	}  
+			gridCell.style.height = cellSideLength + "px";
+			gridCell.style.width = cellSideLength + "px";
+			gridCell.style.borderRadius = 0.4 * cellSpace + "px";
+		}  
+	}
 	// 创建一个二维数组来存放数据
 	for(var i = 0; i < 4; i++) {
 		board[i] = new Array();
@@ -37,38 +46,30 @@ function init() {
 
 // 绘制方格中的数字
 function updateBoardView() {
-	
+
 	// 删除已经绘制好的数字
 	$(".number-cell").remove();
 	for(var i = 0; i < 4; i++) {
 		for(var j = 0; j < 4; j++) {
-			/*
-			var element	= document.createElement("div");
-			element.className = "number-cell";
-			element.id = "number-cell-"+ i + "-" + j;
-			var gridContainer = document.getElementById("grid-container");
-			gridContainer.appendChild(element);
-			*/
 			$("#grid-container").append('<div class="number-cell" id="number-cell-'+ i + '-' + j +'"></div>');
 			var theNumberCell = $("#number-cell-" + i + "-" + j);
-
 			if(board[i][j] == 0) {
 				theNumberCell.css("height", "0px");
 				theNumberCell.css("width", "0px");
-				theNumberCell.css("left", getPos(i, j)[0] + 50);		// 不能加引号 left top的值是数值 不是字符串
-				theNumberCell.css("top", getPos(i, j)[1] + 50);
 			} else {
-				theNumberCell.css("height", "100px");
-				theNumberCell.css("width", "100px");
-				theNumberCell.css("left", getPos(i, j)[0]);		
-				theNumberCell.css("top", getPos(i, j)[1]);
+				theNumberCell.css("height", cellSideLength + "px");
+				theNumberCell.css("width", cellSideLength + "px");
 				theNumberCell.css("background-color", getNumberBackgroundColor(board[i][j]));      // 背景颜色和数字有关系
-				theNumberCell.css("color", getNumberColor(board[i][j]));					    	// 设置字体的颜色 字体颜色随着数字大小的变化而变化
+				theNumberCell.css("color", getNumberColor(board[i][j]));							// 设置字体的颜色 字体颜色随着数字大小的变化
 				theNumberCell.text(board[i][j]);
 			}
+			theNumberCell.css("left", getPos(i, j)[0]);		// 不能加引号 left top的值是数值 不是字符串
+			theNumberCell.css("top", getPos(i, j)[1]);
+			theNumberCell.css("border-radius", 0.4 * cellSpace);
+			theNumberCell.css("line-height", cellSideLength + "px");
 			hasConflicted[i][j] = false;
 		}
-	} 	
+	}
 }
 
 // 棋盘方格块背景颜色判断函数
@@ -157,29 +158,48 @@ function showNumAnimation(x, y, Num) {
 	NumCell.css("color", getNumberColor(Num));	
 	NumCell.text(Num);
 	NumCell.animate({
-		width: "100px",
-		height: "100px",
+		width: cellSideLength + "px",
+		height: cellSideLength + "px",
 		left: getPos(x, y)[0],
 		top: getPos(x, y)[1]
 	}, 200);
 }
 
-//  row   表示方格所在的行  
-// 	col1  表示靠左边的方格(将要移入的位置)
-//  col2  表示靠右边的方格(要移动的方格)
+// 响应式游戏方格大小
+function prepareForMobile() {
+	if(documentWdith > 768) {
+		gridContainerWidth = 500;
+		cellSpace = 20;
+		cellSideLength = 100;
+	}
+	$("#grid-container").css("width", gridContainerWidth - 2 * cellSpace);
+	$("#grid-container").css("height", gridContainerWidth - 2 * cellSpace);
+	$("#grid-container").css("padding", cellSpace);
+	$("#grid-container").css("border-radius", 0.4 * cellSpace)
+}
+
+/*
+* row 表示当前想要比较方格所在的行数
+* col1 起点的数字所在的列数
+* col2 终点的数字所在的列数
+* 判断起点与终点的水平方向之间有没有空格阻碍
+*/
 function noHorizontalBlock(row, col1, col2) {
-	for( var i= col1 + 1; i< col2; i++) {
+	for( var i = col1 + 1; i < col2; i++) {
 		if(board[row][i] != 0) 
 			return false;
 	}
 	return true;
 }
 
-//	col 表示当前想要比较方格所在的列数
-//	row1 起点的数字所在的行数
-// 	row2 终点的数字所在的行数
+/*
+* col 表示当前想要比较方格所在的列数
+* row1 起点的数字所在的行数
+* row2 终点的数字所在的行数
+* 判断起点与终点的垂直方向之间有没有空格阻碍
+*/
 function noVerticalBlock(col, row1, row2) {
-	for( var i= row1 + 1; i< row2; i++) {
+	for( var i = row1 + 1; i < row2; i++) {
 		if(board[i][col] != 0)
 			return false;
 	}
@@ -240,19 +260,23 @@ function moveLeft() {
 			// 当 board[i][j] != 0 的时候, 表示存在一个可以移动的值
 			if(board[i][j] != 0) {
 				for(var k = 0;  k < j; k++) {
+					// 遍历方格中的所有点，判断方格中的点是否存在左边有空格而且两个之间没有其他空格阻碍 
+					// 注意循环遍历的时候要从最左边的开始查找
 					if(board[i][k] == 0 && noHorizontalBlock(i, k, j)) {
 						//	移动方格
-						showMoveAnimation(i, j, i, k);
-						board[i][k] = board[i][j];
-						board[i][j] = 0;
+						showMoveAnimation(i, j, i, k);	
+						board[i][k] = board[i][j];		// 将当前的移动位置的数子传给移动后的方格
+						board[i][j] = 0;				
 						continue;
-					} else if ( board[i][k] == board[i][j] && noHorizontalBlock(i, k, j)  && !hasConflicted[i][k] ){
-						showMoveAnimation(i, j, i, k);
-						board[i][k] += board[i][j];
-						board[i][j] = 0;
-						score += board[i][k];
-						hasConflicted[i][k] = true;
-						updateScore(score);
+					} else if ( board[i][k] == board[i][j] && noHorizontalBlock(i, k, j)  && !hasConflicted[i][k] ) {
+						// 判断是否有两个点相等而且这两个点之间没有其他的空格阻碍， 终点没有重叠过
+						// 设置hasConflicted数组是为了每次移动只能合并一次 消除一个滑动出现多次叠加的问题
+						showMoveAnimation(i, j, i, k);			// 调用移动数字的动画函数
+						board[i][k] += board[i][j];				
+						board[i][j] = 0;						// 将移动后的起点初始化为0
+						score += board[i][k];					// 将变化的值赋值给score分数
+						hasConflicted[i][k] = true;				// 该位置已经重叠过了
+						updateScore(score);						// 将实时变化的分数显示出来
 						continue;
 					}
 				}
@@ -285,8 +309,8 @@ function moveRight() {
 						showMoveAnimation(i, j ,i, k);
 						board[i][k] += board[i][j];
 						board[i][j] = 0;
-						hasConflicted[i][k] = true;
 						score += board[i][k];
+						hasConflicted[i][k] = true;
 						updateScore(score);
 						continue;
 					}
@@ -314,9 +338,9 @@ function moveUp() {
 					} else if (board[k][j] == board[i][j] && noVerticalBlock(j, k, i) && !hasConflicted[k][j]) {
 						showMoveAnimation(i, j, k, j);
 						board[k][j] += board[i][j];
-						board[i][j] = 0;		
-						hasConflicted[k][j] = true;
-						score += board[i][k];	
+						board[i][j] = 0;	
+						score += board[k][j];	
+						hasConflicted[k][j] = true;	
 						updateScore(score);			
 						continue;
 					}
@@ -332,9 +356,6 @@ function moveDown() {
 	if(!canMoveDown()) {
 		return false;
 	}
-	//for(var i= 0; i< 3; i++) {
-		// j= 1 最左边的不用考虑移动
-	//	for(var j= 0; j< 4; j++) {
 	for(var j= 0; j< 4; j++) {
 		for(var i= 2; i>= 0; i--) {
 			if(board[i][j] != 0) {
@@ -348,7 +369,7 @@ function moveDown() {
 						showMoveAnimation(i, j, k, j);
 						board[k][j] += board[i][j];
 						board[i][j] = 0;
-						score += board[i][k];
+						score += board[k][j];
 						hasConflicted[k][j] = true;
 						updateScore(score);
 						break;
@@ -363,21 +384,25 @@ function moveDown() {
 
 // 移动数字节点
 function showMoveAnimation(fromx, fromy, tox, toy) {
-	var numberCell = $('#number-cell-'+fromx+'-'+fromy);
+	var numberCell = $('#number-cell-' + fromx + '-' + fromy);
 	numberCell.animate({
-		top: getPos(tox, toy)[1],
-		left: getPos(tox, toy)[0]
+		left: getPos(tox, toy)[0],
+		top: getPos(tox, toy)[1]
 	}, 300);
 }
 
 // 获取方格位置,返回数组[left,top]
 function getPos(i, j) {
-	return [20 + 120 * j , 20 + 120 * i];
+	return [cellSpace + (cellSideLength + cellSpace) * j , cellSpace + (cellSideLength + cellSpace) * i];
 }
 
-// 可以添加动画功能上去
+// 更新实时游戏分数
 function updateScore(num) {
 	$("#score").text(num);
+	if(score > bestScore) {
+		bestScore = score;
+		$("#best-score").text(bestScore);
+	}
 }
 
 // 判断游戏中是否还有空格,返回false,就表示还有空格
@@ -401,6 +426,7 @@ function noMove() {
 // 判断游戏是否结束
 function isGameOver() {
 	if(noSpace() && noMove()) {
+		
 		gameOver();
 	}
 }
